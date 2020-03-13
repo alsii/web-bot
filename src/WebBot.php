52de454d443a9180185ce78e38a388678d8cf5f0
Aleksei Sitnikov <alsi.0669@gmail.com>
2020-03-13 10:47:39 +0100

    PSR-3 Logger added
    Signed-off-by: Aleksei Sitnikov <alsi.0669@gmail.com>

--- src/WebBot.php
+++ src/WebBot.php
@@ -3,13 +3,25 @@ namespace Alsi\WebBot;
 
 use Alsi\WebBot\Step\StepException;
 use Alsi\WebBot\Step\StepInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 
 class WebBot
 {
+    use LoggerAwareTrait;
+
+    public const LOG_ENTITY_ID = 'entity-id';
+
     /**
      * @var StepInterface[]
      */
     private $steps;
+
+    public function __construct()
+    {
+        $this->logger = new NullLogger();
+    }
 
     /**
      * @param StepInterface $step
@@ -28,15 +40,27 @@ class WebBot
      */
     public function register($state = []): bool
     {
+        $logId = $state[self::LOG_ENTITY_ID] ?? null;
+        $logContext = $logId ? [self::LOG_ENTITY_ID => $logId] : [];
+
         foreach($this->steps as $step) {
-            echo PHP_EOL, '==== Trying ', get_class($step), PHP_EOL;
+            $this->logger->info('==== Trying ' . get_class($step), $logContext);
             try {
                 $state = $step->execute($state);
             } catch (StepException $e) {
-                echo PHP_EOL, 'Step Exception on ', get_class($step), '. Message: ', $e->getMessage(), PHP_EOL;
+                /** @var  $response ResponseInterface */
+                $response = $e->getContext()['response'] ?? null;
+                $responseContent = $response ? $response->getBody()->getContents() : '';
+
+                $this->logger->critical(
+                    'Step Exception(' . $e->getCode() . ') on ' . get_class($step) .
+                    ': ' . $e->getMessage() . PHP_EOL . $responseContent,
+                    $logContext
+                );
+
                 return false;
             }
-            echo PHP_EOL, '---- Done ', get_class($step), PHP_EOL;
+            $this->logger->info('---- Done ' . get_class($step), $logContext);
         }
 
         return true;
